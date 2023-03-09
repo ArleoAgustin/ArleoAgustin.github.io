239dbb844f48dc7db8a51fd77c3e691eebbbf3c9
ArleoAgustin <arleo.agustin20@gmail.com>
2023-03-09 20:52:44 -0300

    add loader + style estructure

---
+++ js/loader.js
@@ -0,0 +1,19 @@
+const loader = document.querySelector(".loader");
+const main = document.querySelector("article")
+const header = document.querySelector("header");
+console.log(main)
+function init(){
+    setTimeout(() =>{
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        main.style.opacity = 1;
+        main.style.display = "block";
+
+        header.style.opacity = 1;
+        header.style.display = "block";
+    }
+    ,Math.random() * (2000 - 500) + 500);
+}
+
+init();
